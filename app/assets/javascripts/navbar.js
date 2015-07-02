7aaf7e85d10ceb271f95767a8c37ff785d84a114
Courtney <courtney.mae.phillips@gmail.com>
2015-07-02 12:48:35 -0700

    Add styling, relevant navbars to individual pages.

--- app/assets/javascripts/navbar.js
+++ app/assets/javascripts/navbar.js
@@ -1,28 +1,15 @@
-$(document.ready(function(){
 
-  $('.menu li a').click(function(e) {
-    var $this = $(this);
-    if (!$this.hasClass('active')) {
-      $this.addClass('active');
-    }
-    e.preventDefault();
-  });
-
-});
-
-
-
-
-var menu = document.querySelector('.menu');
-var anchors = menu.getElementsByTagName('a');
-
-for (var i = 0; i < anchors.length; i += 1) {
-  anchors[i].addEventListener('click', function() { clickHandler(anchors[i]) }, false);
-}
-
-function clickHandler(anchor) {
-  var hasClass = anchor.getAttribute('class');
-  if (hasClass !== 'active') {
-    anchor.setAttribute('class', 'active');
-  }
-}
+// 
+// var menu = document.querySelector('.menu');
+// var anchors = menu.getElementsByTagName('a');
+//
+// for (var i = 0; i < anchors.length; i += 1) {
+//   anchors[i].addEventListener('click', function() { clickHandler(anchors[i]) }, false);
+// }
+//
+// function clickHandler(anchor) {
+//   var hasClass = anchor.getAttribute('class');
+//   if (hasClass !== 'active') {
+//     anchor.setAttribute('class', 'active');
+//   }
+// }
