c52ae25bf2bc98d9fbf8fbc3a4bc38845690966b
Courtney <courtney.mae.phillips@gmail.com>
2015-07-10 08:31:12 -0700

    Add posts table to database.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150710151402) do
+ActiveRecord::Schema.define(version: 20150710153028) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,11 @@ ActiveRecord::Schema.define(version: 20150710151402) do
     t.string   "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.string "content"
   end
 
   create_table "projects", force: :cascade do |t|
