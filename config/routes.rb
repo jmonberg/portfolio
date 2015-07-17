ac862bb231a7ac7cbf5a3e04cc0d4ca8458c94a9
Courtney <courtney.mae.phillips@gmail.com>
2015-07-17 10:51:49 -0700

    Add references.

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
   root :to => 'categories#index'
-
   resources :categories do
     resources :projects
   end
-
   resources :posts do
     resources :comments
   end
 
+  resources :references
+
   get '/about', to: 'pages#about'
   get '/denied', to: 'pages#denied'
-
 end
