7f55bb384fafde1fc140464e5f2dda149ca0dd13
Courtney <courtney.mae.phillips@gmail.com>
2015-07-17 11:09:10 -0700

    Migrate references.

---
+++ db/migrate/20150717180727_add_references.rb
@@ -0,0 +1,10 @@
+class AddReferences < ActiveRecord::Migration
+  def change
+    create_table :references do |t|
+      t.string :name
+      t.text :body
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150710192727) do
+ActiveRecord::Schema.define(version: 20150717180727) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,13 @@ ActiveRecord::Schema.define(version: 20150710192727) do
     t.string   "description"
     t.string   "github"
     t.integer  "category_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "references", force: :cascade do |t|
+    t.string   "name"
+    t.text     "body"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
